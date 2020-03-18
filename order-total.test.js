3b7401ff5f14bbf44aaa2e0744041d8ed925f50b
rosanaemidio <rosanaemidio42@gmail.com>
2020-03-18 13:30:42 -0300

    Add updates

--- order-total.test.js
+++ order-total.test.js
@@ -8,13 +8,23 @@ it('calls vaptapi.com correctly', () => {
     const fakeFetch = (url) => {
         expect(url).toBe('https://vatapi.com/v1/country-code-check?code=DE')
         isFakeFetchCalled = true
+        return Promise.resolve({
+          json: () => Promise.resolve({
+            rates: {
+              standard:{
+                 value: 19
+              }
+            }
+          })
+        })
     }
-    orderTotal(fakeFetch, {
+    return orderTotal(fakeFetch, {
         country: 'DE',
         items: [
             { 'name': 'Dragon Waffles', price: 20, quantity: 2 }
         ]
     }).then(result => {
+      expect(result).toBe(20*2*1.19)
         expect(isFakeFetchCalled).toBe(true)
     })
 })
--- orderTotal.js
+++ orderTotal.js
@@ -1,5 +1,9 @@
 function orderTotal(fetch , order) {
     fetch('https://vatapi.com/v1/country-code-check?code=' + order.country)
+     .then(response => response.json())
+     .then(data => data.rates.standard.value)
+     .then(vat => order.items.reduce((prev, cur) => 
+        cur.price * (cur.quantity || 1)+ prev,0)* vat)
     return Promise.resolve(order.items.reduce((prev, cur)=>
      cur.price * (cur.quantity || 1) + prev, 0))  
 }
